55ffea1949082031b7ed383c34e36ee489533655
FaustXVI <xavier.detant@gmail.com>
2017-03-19 16:24:03 +0100

    Ready for demo

--- mongodb/src/test/java/com/github/awesome/quiz/AnswerRepositoryIT.java
+++ mongodb/src/test/java/com/github/awesome/quiz/AnswerRepositoryIT.java
@@ -1,10 +1,8 @@
 package com.github.awesome.quiz;
 
-import com.github.junit5docker.Docker;
-import com.github.junit5docker.Port;
-import com.github.junit5docker.WaitFor;
 import com.mongodb.MongoClient;
 import com.mongodb.client.MongoDatabase;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -12,18 +10,22 @@ import static com.github.awesome.quiz.Answer.answer;
 import static com.github.awesome.quiz.Question.question;
 import static org.assertj.core.api.Assertions.assertThat;
 
-@Docker(image = "mongo",
-    ports = @Port(exposed = 27017, inner = 27017),
-    waitFor = @WaitFor("waiting for connections on port"))
 public class AnswerRepositoryIT {
+
+    private MongoDatabase database;
 
     private AnswerRepository repository;
 
     @BeforeEach
     public void createDatabase() {
-        MongoClient mongoClient = new MongoClient("localhost");
-        MongoDatabase database = mongoClient.getDatabase("awesome-quiz");
+        MongoClient mongoClient = new MongoClient("my-awesome-mongo-in-cloud");
+        database = mongoClient.getDatabase("awesome-quiz");
         repository = new AnswerRepository(database);
+    }
+
+    @AfterEach
+    public void deleteDatabase() {
+        database.drop();
     }
 
     @Test
