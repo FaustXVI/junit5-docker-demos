16e3b18c8fbed8901ab7d17ea127a1728c258dd0
FaustXVI <xavier.detant@gmail.com>
2017-03-26 20:42:59 +0200

    Ready for demo

--- mongodb/src/test/java/com/github/awesome/quiz/AnswerRepositoryIT.java
+++ mongodb/src/test/java/com/github/awesome/quiz/AnswerRepositoryIT.java
@@ -1,8 +1,5 @@
 package com.github.awesome.quiz;
 
-import com.github.junit5docker.Docker;
-import com.github.junit5docker.Port;
-import com.github.junit5docker.WaitFor;
 import com.mongodb.MongoClient;
 import com.mongodb.client.MongoDatabase;
 import org.junit.jupiter.api.BeforeEach;
@@ -12,16 +9,13 @@ import static com.github.awesome.quiz.Answer.answer;
 import static com.github.awesome.quiz.Question.question;
 import static org.assertj.core.api.Assertions.assertThat;
 
-@Docker(image = "mongo",
-    ports = @Port(exposed = 27017, inner = 27017),
-    waitFor = @WaitFor("waiting for connections on port"))
 public class AnswerRepositoryIT {
 
     private AnswerRepository repository;
 
     @BeforeEach
     public void createDatabase() {
-        MongoClient mongoClient = new MongoClient("localhost");
+        MongoClient mongoClient = new MongoClient("my-awesome-cloud");
         MongoDatabase database = mongoClient.getDatabase("awesome-quiz");
         repository = new AnswerRepository(database);
     }
